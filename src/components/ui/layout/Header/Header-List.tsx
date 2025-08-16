dad5c3005ff7aecdd3e0d689e376772f55861fbe
22136445989 <accdlyatarkova@gmail.com>
2025-08-16 05:52:08 +0300

    Header-List added

---
+++ src/components/ui/layout/Header/Header-List.tsx
@@ -0,0 +1,17 @@
+import './Header-Style.scss';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <ul className="List">
+        <li>Игры</li>
+        <li>Поддержка</li>
+        <li>FAQ</li>
+        <li>Турниры</li>
+        <li>Джекпот</li>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
